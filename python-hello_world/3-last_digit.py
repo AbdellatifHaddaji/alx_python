925b270dfad3df74c0bd3abd6c77c7ab3258df5f
AbdellatifHaddaji <haddaji.abdellatif98@gmail.com>
2023-11-28 09:55:06 +0100

    python-hello_world

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,11 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-x = abs(number) % 10
+x = number % 10 if number >= 0 else -(-number % 10)
 if x > 5:
-  print('Last digit of',number, 'is', x,' and is greater than 5')
+  print('Last digit of',number, 'is', x,'and is greater than 5')
 if x == 0:
-  print('Last digit of',number, 'is', x,' and is 0')
+  print('Last digit of',number, 'is', x,'and is 0')
 if x < 6 and x !=0:
-  print('Last digit of',number, 'is', x,' and is less than 6 and not 0')
+  print('Last digit of',number, 'is', x,'and is less than 6 and not 0')
 
--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,3 @@
 for i in range(10):
     for j in range(i + 1, 10):
-        if i != j:
-            if i < 9:
-                print("{:d}{:d}, ".format(i, j), end="")
-            else:
-                print("{:d}{:d}".format(i, j))
+        print("{:02d}".format(int(str(i) + str(j))), end=", " if (i, j) != (8, 9) else "\n")
