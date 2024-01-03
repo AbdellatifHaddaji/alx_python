f5cbd3cbf48011d43a352eeccba23e3defbeda49
AbdellatifHaddaji <haddaji.abdellatif98@gmail.com>
2024-01-03 21:47:14 +0100

    python-inheritance

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,16 +1,6 @@
 def is_same_class(obj, a_class):
     """
-    Returns True if obj is exactly an instance of a_class; otherwise, returns False.
+    Returns True if obj is exactly an instance of the specified class;
+    otherwise, returns False.
     """
-    return type(obj) is a_class
-
-# Example usage
-if __name__ == "__main__":
-    a = 1
-
-    if is_same_class(a, int):
-        print("{} is an instance of the class {}".format(a, int.__name__))
-    if is_same_class(a, float):
-        print("{} is an instance of the class {}".format(a, float.__name__))
-    if is_same_class(a, object):
-        print("{} is an instance of the class {}".format(a, object.__name__))
+    return type(obj) == a_class
--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,42 +1,16 @@
 class BaseGeometry:
-    """BaseGeometry class with area and integer_validator methods."""
     def area(self):
-        """Raises an Exception with the message 'area() is not implemented'."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validates the value:
-        - If value is not an integer: raise a TypeError exception with the message '<name> must be an integer'
-        - If value is less or equal to 0: raise a ValueError exception with the message '<name> must be greater than 0'
-        """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
-    """Rectangle class inheriting from BaseGeometry."""
     def __init__(self, width, height):
-        """Instantiation with width and height."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-# Example usage
-if __name__ == "__main__":
-    r = Rectangle(3, 5)
-
-    print(r)
-    print(dir(r))
-
-    try:
-        print("Rectangle: {} - {}".format(r.__width, r.__height))
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        r2 = Rectangle(4, True)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
