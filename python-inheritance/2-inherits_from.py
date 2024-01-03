df116ede7fb4bfbcdc66c022a3fe226e1a6e0661
AbdellatifHaddaji <haddaji.abdellatif98@gmail.com>
2024-01-03 21:52:23 +0100

    python-inheritance

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,2 +1,3 @@
 def inherits_from(obj, a_class):
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    """Check if obj is an instance of a class that inherited from the specified class"""
+    return issubclass(type(obj), a_class)
--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,22 +1,30 @@
 class BaseGeometry:
+    """Base class with an integer validator method"""
+
     def area(self):
+        """Raise an exception indicating that area() is not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validate that value is an integer and greater than 0"""
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
+    """Rectangle class inheriting from BaseGeometry"""
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
-    def area(self):
-        return self.__width * self.__height
-
     def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        """Return a string representation of the rectangle"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+        """Compute the area of the rectangle"""
+        return self.__width * self.__height
