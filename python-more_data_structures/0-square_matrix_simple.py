4e03a3a985957e20f7a8133e356be413c7e04765
AbdellatifHaddaji <haddaji.abdellatif98@gmail.com>
2024-01-10 14:35:10 +0100

    python-more_data_structure

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,18 +1,19 @@
 def square_matrix_simple(matrix=[]):
-    # Create a new matrix with the same size as the input matrix
     new_matrix = []
     for row in matrix:
-        new_row = [x ** 2 for x in row]
+        new_row = []
+        for col in row:
+            new_row.append(col ** 2)
         new_matrix.append(new_row)
     return new_matrix
 
-# Test case
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,15 +1,3 @@
 def best_score(a_dictionary):
     if a_dictionary:
-        # Find the key with the maximum value
-        best_key = max(a_dictionary, key=a_dictionary.get)
-        return best_key
-    else:
-        return None
-
-# Test cases
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
+        return max(a_dictionary, key=a_dictionary.get)
