878d03f5f48bfcfc2350c1cb269979ab0040294a
AbdellatifHaddaji <haddaji.abdellatif98@gmail.com>
2024-01-10 14:43:37 +0100

    python-almost_a_circle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,14 +1,23 @@
-#!/usr/bin/python3
-""" Base Module """
+"""
+A module that has a class named base which will be 
+the “base” of all other classes in this project. 
+The goal of it is to manage id attribute in all your future classes and 
+to avoid duplicating the same code 
+"""
 
 class Base:
-    """ Base class for all other classes """
+    """
+    A class that have:
+    - Private attribute 
+    - class constructor
+    """
     __nb_objects = 0
-
     def __init__(self, id=None):
-        """ Initializes a new instance of Base """
+        """
+        A class constructor with argument id
+        """
         if id is not None:
-            self.id = id
+            self.id = id 
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.id = Base.__nb_objects 
--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,20 +1,32 @@
-
-""" Square Module """
+"""
+A module that have a class Square that inherits from Rectangel
+"""
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
-    """ Square class inherits from Rectangle """
+    """
+    A class that inherits from class Rectangle
+    Square class has constructor class
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """ Initializes a new instance of Square """
-        super().__init__(size, size, x, y, id)
+        """
+        A constructor class with Rectangle arguments
+        """
+        super().__init__(size, size,x,y,id)
 
+    def __str__(self) -> str:
+        """
+        A method that  overriding the __str__ method so that it returns 
+        [Square] (<id>) <x>/<y> - <size>
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id,self.x,self.y,self.width)
     @property
     def size(self):
-        """ Getter for size """
         return self.width
-
     @size.setter
-    def size(self, value):
-        """ Setter for size """
-        self.width = value
-        self.height = value
+    def size(self,width):
+        if not isinstance(width,int):
+            raise TypeError("width must be an integer")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
+        self.width = width
